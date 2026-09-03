a19f62926bb8cc1cb88d6745d16b493eb9670fcf
Christian Guizinho <christian@example.com>
2026-09-03 18:49:37 -0300

    dados supabase

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from
-    "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+    "";
 
 const SUPABASE_URL =
-    "https://ojbzshcwuqkzlvqwybxh.supabase.co";
+    "";
 
 const SUPABASE_KEY =
     "sb_publishable_8o9mtbdQmr6x8jBG699i9g_6JNkcGFC";
